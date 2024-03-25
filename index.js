582c793061d6a03934e4b5a60acfc0253303d9f9
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-03-25 14:37:38 +0530

    Data Types

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 console.log('Hello World in JS Browser');
 console.log('node index.js to run in the Node environment');
+
+let name  = 'shehan jayasooriya';
+let age   = 20;
+let isMarried = true;
+let middleName = undefined;
+let selectColor = null;
+
+
+console.log('Types of Variables');
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(middleName));
+console.log(typeof(selectColor));
+
+
