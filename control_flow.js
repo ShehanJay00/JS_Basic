1c7bc3d8fd8c5f8943149f8873ea4e0ff5965bbf
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-03-27 17:50:42 +0530

    loops

--- control_flow.js
+++ control_flow.js
@@ -27,3 +27,53 @@ switch(role){
         console.log('Unknown User');
         break;
 }
+
+
+//Loops - for loop
+for(let i = 0 ; i < 10 ; i++){
+    console.log("Itteration ",i);
+}
+
+console.log();
+
+for(let i = 0 ; i < 10 ; i++){
+    if(i%2 !==0)
+        console.log("Odd number ",i);
+}
+
+
+console.log();
+
+
+//Loops - while loop
+let k = 0;
+while(k < 21){
+    console.log(k);
+k++;
+}
+
+console.log();
+
+//Loops - do while loop
+let j =13;
+do{
+    if(j%2 !== 0)console.log(j);
+    j++;
+}while(j<=5);
+
+
+console.log();
+
+//Loops - for in loops
+const student ={
+    name :'shehan',
+    age  : '24',
+}
+for(let key in student)console.log(key,student[key]);
+
+
+console.log();
+
+//Loops - for of loops
+const colors = ['blue','red','yellow','green'];
+for(let color of colors)console.log(color);
