43ba1a8cabe431f3530aff6d384dd2f0a8ad82a4
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-04-02 19:29:07 +0530

    Array Sorting

--- arrays.js
+++ arrays.js
@@ -36,3 +36,53 @@ console.log(course_2_Index)
 
 const course_2 = courses.find(course => course.name === 'IBM')
 console.log(course_2)
+
+
+console.log()
+
+// Iterating an Array
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
+for (let n of nums)
+    console.log(n)
+
+
+nums.forEach(number => {
+    console.log(number);
+});
+
+console.log()
+
+// Num with ID
+nums.forEach((number,index) => {
+    console.log(number,index);
+});
+
+
+// Joining arrays
+const msg = 'this is my message for join'
+const parts = msg.split(' ')
+console.log(parts)
+
+const combined = parts.join('-')
+console.log(combined)
+
+
+
+// Array Sorting
+
+const languages = [
+    {id:1 ,name: 'javescript'},
+    {id:2 ,name: 'Bnode'},
+];
+
+languages.sort(function(one,two){
+    const nameOne = one.name.toUpperCase();
+    const nameTwo = two.name.toUpperCase();
+
+    if(nameOne < nameTwo) return -1
+    if(nameOne > nameTwo) return 1
+    return 0
+});
+
+console.log(languages);
